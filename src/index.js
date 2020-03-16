f3c6ea06f3ee80f69116b97656a32f4e388cb6fe
Dedenich <sergbrst@tut.by>
2020-03-16 11:05:09 +0300

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let newStr = str;
+    let length;
+    let configArr= [];
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        let element = bracketsConfig[i].map(
+                        val => Number.isInteger(+val) ? val : '\\' + val
+                    ).join('');
+
+        configArr.push(element);   
+    }
+
+    let regexp = new RegExp(`(${configArr.join('|')})`, 'g');
+
+    do {
+        length = newStr.length;
+        newStr = newStr.replace(regexp, '');
+    } while (length !== newStr.length);
+
+    return newStr.length ? false : true;
 }
